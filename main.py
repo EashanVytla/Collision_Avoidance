73a2667dada9c2ddaac96d819d79e6c5fb9a4cce
bworkman503 <bworkman503@gmail.com>
2024-01-16 12:34:09 -0500

    Implemented Basic Algo
    Created functions to get RVs, transformation matrix, then RAVs. Velocity maneuver is chosen randomly from RAVs.

--- main.py
+++ main.py
@@ -7,41 +7,72 @@ rpz = 0.0
 
 def init():
     rpz = sim.sphere_size
-
     return
 
-def get_az(v): #TEST/FIX
-    v[0, 2] = 0
-    az = np.arctan(v[0,1]/v[0,0])
+def get_transform_matrix(doi): #Fails if in same pos - doi = 0 #Trig matrix didnt seem to work idk
+    e1 = doi / np.linalg.norm(doi)
+    e2 = np.zeros((3,1))
+    m = np.nonzero(e1)[0][0]
+    if (m > 0):
+        n = 0
+    else:
+        n = 1
+    e2[n,0] = e1[m,0]
+    e2[m,0] = -1*e1[n,0]
+    e2 = e2 / np.linalg.norm(e2)
+    e3 = np.cross(e1.T, e2.T).T
+    transform = np.concatenate((e1,e2,e3),1)
+    transform = np.linalg.inv(transform)
+    return transform
 
-    return az
+def get_RV():
+    phi_range = np.arange(0,22)*2*np.pi/22
+    theta_range = np.arange(1,11)*np.pi/11
+    FAunit = np.array([[0,0,1],[0,0,-1]]).T
+    for phi in phi_range:
+        for theta in theta_range:
+            v = np.array([[np.sin(theta)*np.cos(phi),np.sin(theta)*np.sin(phi),np.cos(theta)]]).T
+            FAunit = np.concatenate((FAunit, v),1)
+    delta_t = 1 #not sure what timestep to use - also consider shifting own & occ pos by v*timestep for accuracy
+    accel_max = 10 #no clue
+    FA = FAunit.copy()
+    for k in range(2,(accel_max)+1):
+        FA = np.concatenate((FA, FAunit*k),1)
+    FA = np.concatenate((FA, np.array([[0],[0],[0]])),1)
+    RV = FA*delta_t + sim.get_own_velo().T
+    return RV
 
-def get_el(v): #TEST/FIX
-    h = v
-    h[0, 2] = 0
-    h = np.linalg.norm(h)
-    el = np.arctan(v[0,2]/h)
+def get_RAV(doi, rvo, dvo):
+    RV = get_RV()
+    RRV_transform = get_transform_matrix(doi) @ (RV-sim.get_occ_velo().T)
+    RAV = np.array([[],[],[]])
+    for j in range(0, np.shape(RRV_transform)[1]):
+        if (RRV_transform[0,j] <= 0 or np.sqrt(pow(RRV_transform[1,j],2)+pow(RRV_transform[2,j],2))/RRV_transform[0,j] >= rvo/dvo):
+            RAV = np.concatenate((RAV, np.array([RV[:,j]]).T),1)
+    return RAV
 
-    return el
+def select_velo(RAV): #random right now
+    rand = np.random.randint(0, np.shape(RAV)[1])
+    velo = np.array([RAV[:,rand]]) #horizontal
+    return velo
+
 
 def main_loop():
-    doi = sim.get_occ_pos()[0,:] - sim.get_own_pos()[0,:]
-    doi_norm = np.linalg.norm(doi)
-    if (doi_norm == 0):
-        doi_norm = 0.1
-
-    rvo = rpz*((math.sqrt(pow(doi_norm, 2) - pow(rpz, 2)))/doi_norm)
+    doi = (sim.get_occ_pos() - sim.get_own_pos()).T
+    doi_norm = np.linalg.norm(doi) #problem if same pos - doi = 0
+    rvo = rpz*((np.sqrt(pow(doi_norm, 2) - pow(rpz, 2)))/doi_norm)
     dvo = (pow(doi_norm, 2) - pow(rpz, 2))/doi_norm
-    #theta_az = get_az(sim.get_occ_pos()[0,:])
-    #theta_el = get_el(sim.get_occ_pos()[0,:])
-
-    testAZ = get_az(np.array([[-1,0,0]]))
-    testEL = get_el(np.array([[-1,0,0]]))
-    testAZ = np.rad2deg(testAZ)
-    testEL = np.rad2deg(testEL)
-    print(testAZ)
-    #print(testEL)
     
+    RAV = get_RAV(doi, rvo, dvo)
+    velo = select_velo(RAV)
+
+    sim.set_own_velo(velo/20) #/20 cuz idk how sim function works
+
+
+    
+
+
+
     # if(sim.get_own_pos()[0, 0] > 25):
     #     sim.set_own_velo(np.array([0, 1, 0]))
     # else:
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import numpy as np
 
-i = np.array([[1, 2, 3], [5, 6, 7]])
-print(i[:, 2])
+a = np.array([[1,2,3],[4,5,6]]).T
+b = np.array([[],[],[]])
+c = np.concatenate((b, a),1)
+print(c)
+print(np.shape(c))
