a4776839daa4562b717478a53d28f5b0f8f2b37d
bworkman503 <bworkman503@gmail.com>
2024-01-17 21:14:37 -0500

    Completed mode check and RMA
    Added code to detect collision imminence (distance and time), collision, and divergence. Mode is determined from these factors. Implemented restore, maintain, avoid mode switching.

--- main.py
+++ main.py
@@ -31,14 +31,16 @@ def main_loop():
     transform = np.concatenate((e1,e2,e3),1)
     transform = np.linalg.inv(transform)
 
-#For each obstacle, determine: t-imminence, d-imminence, divergence, collision
+    #For each obstacle, determine: t-imminence, d-imminence, divergence, collision
     #MODE DETERMINATION
     #if d non-imminent, t non-imminent: restore
     #if d imminent, t imminent: avoid
     #if d non-imminent, t imminent: avoid
     #if d imminent, t non-imminent: 1) divergent - restore 2) non-divergent: a) non-colliding: maintain b) colliding: avoid
     
-    #Select Mode #FINISH THIS PART
+    #Select Mode #NEED TO TEST/VERIFTY CODE
+
+    #Check t_imminent and colliding
     t_horizon = 10 #10seconds
     d_horizon = 20 #20meters
     velo_rel_transform = transform @ (sim.get_own_velo()-sim.get_occ_velo()).T
@@ -48,45 +50,87 @@ def main_loop():
     elif(np.linalg.norm(velo_rel_transform) > (doi_norm/t_horizon)): #technically should be shortest distance not doi
         colliding = True
         t_imminent = True
+        mode = 2 #avoid
     else:
         colliding = True
         t_imminent = False
-    #maybe put below in if(t_imminent==false): to skip unnecessary checks
-    if (np.linalg.norm(velo_rel_transform) <= (doi_norm/t_horizon)): #technically should be shortest distance not doi
-        d_imminent = False
+
+    #Check d_imminent and diverging
+    if (t_imminent == False):
+        if (np.linalg.norm(velo_rel_transform) <= (doi_norm/t_horizon)): #technically should be shortest distance not doi
+            d_imminent = False
+            mode = 0 #restore
+        else:
+            d_imminent = True
+
+            #Check diverging #Current divergence check idea: transform absolute velocity to cc cone frame, take the coordinate point the velo vector lands at, place the transformed obstacle velocity vector (infinite) backwards at this point and see if it intersects the cc cone
+            startpoint = transform @ sim.get_own_velo().T
+            dir_vector = -1*(transform @ sim.get_occ_velo().T) / np.linalg.norm(sim.get_occ_velo())
+            xv = startpoint[0,0]
+            yv = startpoint[1,0]
+            zv = startpoint[2,0]
+            xn = dir_vector[0,0]
+            yn = dir_vector[1,0]
+            zn = dir_vector[2,0]
+            denom = ((rvo/dvo)**2)*(xn**2)-(yn**2)-(zn**2)
+            numer = ((rvo/dvo)**2) * ((((zv*xn)-(zn*xv))**2)+(((yv*xn)-(yn*xv))**2)) - ((zv*yn)-(zn*yv))**2
+            d1 = ((zn*zv + yn*yv - ((rvo/dvo)**2)*xn*xv) / denom) + (np.sqrt(numer)/denom)
+            d2 = ((zn*zv + yn*yv - ((rvo/dvo)**2)*xn*xv) / denom) - (np.sqrt(numer)/denom)
+            if (denom == 0 or numer < 0):
+                divergence = True
+                mode = 0 #restore
+            elif (d1 >= 0 and (xv+(d1*xn)) >= 0):
+                divergence = False
+            elif (d2 >= 0 and (xv+(d2*xn)) >= 0):
+                divergence = False
+            else:
+                divergence = False
+            if (divergence == False and colliding == False):
+                mode = 1 #maintain
+            if (divergence == False and colliding == True):
+                mode = 2 #avoid
+
+
+
+    #AVOIDANCE MODE
+    if (mode == 2):
+        #Calc RV
+        phi_range = np.arange(0,22)*2*np.pi/22
+        theta_range = np.arange(1,11)*np.pi/11
+        FAunit = np.array([[0,0,1],[0,0,-1]]).T
+        for phi in phi_range:
+            for theta in theta_range:
+                v = np.array([[np.sin(theta)*np.cos(phi),np.sin(theta)*np.sin(phi),np.cos(theta)]]).T
+                FAunit = np.concatenate((FAunit, v),1)
+        delta_t = 1 #not sure what timestep to use - also consider shifting own & occ pos by v*timestep for accuracy
+        accel_max = 8 #8.5 in m/s^2 (rounded for int)
+        FA = FAunit.copy()
+        for k in range(2,(accel_max)+1):
+            FA = np.concatenate((FA, FAunit*k),1)
+        RV = FA*delta_t + sim.get_own_velo().T
+
+        #Calc RAV
+        RRV_transform = transform @ (RV-sim.get_occ_velo().T)
+        RAV = np.array([[],[],[]])
+        for j in range(0, np.shape(RRV_transform)[1]):
+            if (RRV_transform[0,j] <= 0 or np.sqrt(pow(RRV_transform[1,j],2)+pow(RRV_transform[2,j],2))/RRV_transform[0,j] >= rvo/dvo):
+                RAV = np.concatenate((RAV, np.array([RV[:,j]]).T),1)
+                
+        #Select Velocity     #random right now
+        rand = np.random.randint(0, np.shape(RAV)[1])
+        velo = np.array([RAV[:,rand]]) #horizontal
+
+        sim.set_own_velo(velo/20) #/20 cuz idk how sim function works
+    
+    #MAINTAIN MODE
+    elif (mode == 1):
+        sim.set_own_velo(sim.get_own_velo())
+    
+    #RESTORE MODE #technically gives control to path planner, I just pointed to coord 50,0,0 for sim purpose
     else:
-        d_imminent = True
-    #ADD DIVERGENCE CHECK
-        #Current divergence check idea: transform absolute velocity to cc cone frame, take the coordinate point the velo vector lands at, place the obstacle velocity vector (infinite) backwards at this point and see if it intersects the cc cone
-
-
-    #Calc RV
-    phi_range = np.arange(0,22)*2*np.pi/22
-    theta_range = np.arange(1,11)*np.pi/11
-    FAunit = np.array([[0,0,1],[0,0,-1]]).T
-    for phi in phi_range:
-        for theta in theta_range:
-            v = np.array([[np.sin(theta)*np.cos(phi),np.sin(theta)*np.sin(phi),np.cos(theta)]]).T
-            FAunit = np.concatenate((FAunit, v),1)
-    delta_t = 1 #not sure what timestep to use - also consider shifting own & occ pos by v*timestep for accuracy
-    accel_max = 8 #8.5 in m/s^2 (rounded for int)
-    FA = FAunit.copy()
-    for k in range(2,(accel_max)+1):
-        FA = np.concatenate((FA, FAunit*k),1)
-    RV = FA*delta_t + sim.get_own_velo().T
-
-    #Calc RAV
-    RRV_transform = transform @ (RV-sim.get_occ_velo().T)
-    RAV = np.array([[],[],[]])
-    for j in range(0, np.shape(RRV_transform)[1]):
-        if (RRV_transform[0,j] <= 0 or np.sqrt(pow(RRV_transform[1,j],2)+pow(RRV_transform[2,j],2))/RRV_transform[0,j] >= rvo/dvo):
-            RAV = np.concatenate((RAV, np.array([RV[:,j]]).T),1)
-            
-    #Select Velocity     #random right now
-    rand = np.random.randint(0, np.shape(RAV)[1])
-    velo = np.array([RAV[:,rand]]) #horizontal
-
-    sim.set_own_velo(velo/20) #/20 cuz idk how sim function works
+        velo = np.array([[50, 0, 0]]) - sim.get_own_pos()
+        velo = (velo / np.linalg.norm(velo)) * 1
+        sim.set_own_velo(velo)
 
 
     
